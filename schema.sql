b18eca2d8e90c4bf0caf464f21744bd5e8d86935
DarkxDev <bryanfernandohurtado@gmail.com>
2023-03-27 17:42:58 -0500

    Created database, table, and queries

--- schema.sql
+++ schema.sql
@@ -3,3 +3,15 @@
 CREATE TABLE animals (
     name varchar(100)
 );
+
+/* Vet Clinic */
+
+CREATE DATABASE vet_clinic;
+CREATE TABLE animals(
+    id int primary key,
+    name varchar,
+    date_of_birth date,
+    escape_attempts int,
+    neutered bit,
+    weight_kg decimal
+);
